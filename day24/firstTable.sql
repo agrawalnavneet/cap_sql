bc13978ed54874d9204cf688b3e2ecf0348393d9
Navneet Agarwal <agrawalnavneet960@gmail.com>
2026-01-24 10:48:58 +0530

    added tables

--- day24/firstTable.sql
+++ day24/firstTable.sql
@@ -10,21 +10,34 @@ VALUES
 ('Rahul', 21, 'Delhi'),
 ('Neha', 22, 'Mumbai');
 
-SELECT * FROM Student1;
-
+SELECT * FROM Student;
 
 CREATE TABLE Student1 (
     id INT IDENTITY(1,1) PRIMARY KEY,
-    name VARCHAR(100),
+    student_id INT,
     department VARCHAR(50),
-    subject VARCHAR(50)
+    subject VARCHAR(50),
+    FOREIGN KEY (student_id) REFERENCES Student(student_id)
 );
 
-INSERT INTO Student1 (name, department, subject)
+INSERT INTO Student1 (student_id, department, subject)
 VALUES
-('Amit', 'CSE', 'DBMS'),
-('Riya', 'IT', 'Operating Systems');
+(1, 'CSE', 'DBMS'),
+(2, 'IT', 'Operating Systems');
+
 
 SELECT * FROM Student1;
 
-DROP TABLE Student;
+DROP TABLE Student1;
+
+
+SELECT 
+    s.student_name,
+    s.age,
+    s.city,
+    s1.department,
+    s1.subject
+FROM Student s
+JOIN Student1 s1
+ON s.student_id = s1.student_id;
+
